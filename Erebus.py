8a08c7a88ca05bf254867720803083f7cf0c1d88
Joe <joeerwatson@gmail.com>
2022-12-21 17:02:52 +0000

    Added Code

---
+++ Erebus.py
@@ -0,0 +1,67 @@
+import argparse
+import sys
+from PIL import Image
+
+def cut_image(image_file, cut_type="half"):
+    """
+    Cut an image file into half or quarters and save the sections as new files.
+
+    Parameters:
+    - image_file (str): The file path of the image to be cut.
+    - cut_type (str, optional): The type of cut to be performed. Can be "half" or "quarter". Default is "half".
+
+    Returns:
+    None
+    """
+    # Open the image file
+    try:
+        image = Image.open(image_file)
+    except IOError:
+        print("Error opening image file")
+        sys.exit()
+
+    # Get the size of the image
+    width, height = image.size
+
+    # Cut the image into half or quarters
+    if cut_type == "quarter":
+        # Cut the image into quarters
+        left_top = image.crop((0, 0, width//2, height//2))
+        right_top = image.crop((width//2, 0, width, height//2))
+        left_bottom = image.crop((0, height//2, width//2, height))
+        right_bottom = image.crop((width//2, height//2, width, height))
+
+        # Save the quarters as new image files
+        left_top.save(f"{image_file.split('.')[0]}_top_left.jpg")
+        right_top.save(f"{image_file.split('.')[0]}_top_right.jpg")
+        left_bottom.save(f"{image_file.split('.')[0]}_bottom_left.jpg")
+        right_bottom.save(f"{image_file.split('.')[0]}_bottom_right.jpg")
+    else:
+        # Cut the image into half
+        left = image.crop((0, 0, width//2, height))
+        right = image.crop((width//2, 0, width, height))
+
+        # Save the halves as new image files
+        left.save(f"{image_file.split('.')[0]}_left.jpg")
+        right.save(f"{image_file.split('.')[0]}_right.jpg")
+
+    # Close the image file
+    image.close()
+
+def main():
+    # Set up the argument parser
+    parser = argparse.ArgumentParser(description="Cut an image file into half or quarters and save the sections as new files.")
+    parser.add_argument("image_file", help="The file path of the image to be cut.")
+    parser.add_argument("-t", "--type", default="half", choices=["half", "quarter"], help="The type of cut to be performed. Can be 'half' or 'quarter'. Default is 'half'.")
+
+    # Parse the arguments
+    args = parser.parse_args()
+    image_file = args.image_file
+    cut_type = args.type
+
+    # Cut the image
+    cut_image(image_file, cut_type)
+
+# Run the main function
+if __name__ == "__main__":
+    main()
---
+++ README.md
@@ -0,0 +1,42 @@
+# Image Cutter
+
+This Python script takes an image file and cuts it into half or quarters, saving the sections as new image files.
+
+The requirements for this script are:
+
+``
+    Python 3
+``
+
+``
+    Pillow (Python Imaging Library)
+``
+
+To easily get the libraries needed, run this command:
+
+`` pip install -r requirements.txt ``
+
+## Usage
+
+To use the script, run the following command:
+
+`` Erebus.py image_file -t cut_type ``
+
+Replace image_file with the file path of the image to be cut, and cut_type with the type of cut to be performed (either "half" or "quarter"). The cut_type argument is optional; if not specified, the image will be cut into halves.
+Examples
+
+To cut an image called "image.jpg" into halves:
+
+`` Erebus.py image.jpg ``
+
+To cut the image into quarters:
+
+`` Erebus.py image.jpg -t quarter ``
+
+## Output
+
+The script will save the sections of the image as new image files with the original image file name and either "left", "right", "top_left", "top_right", "bottom_left", or "bottom_right".
+Notes
+
+##### The script uses the Python Imaging Library (PIL) to open and manipulate the image file.
+##### The script will exit if it encounters an error when opening the image file.
